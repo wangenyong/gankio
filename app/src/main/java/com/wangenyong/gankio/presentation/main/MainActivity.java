400c6f940d3002fc2017049161d06fc7ee7ba5e6
wangenyong <wangenyong@gmail.com>
2017-03-02 11:48:39 +0800

    Create mvp folder

--- app/src/main/java/com/wangenyong/gankio/presentation/main/MainActivity.java
+++ app/src/main/java/com/wangenyong/gankio/presentation/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.wangenyong.gankio;
+package com.wangenyong.gankio.presentation.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.wangenyong.gankio.R;
 
 public class MainActivity extends AppCompatActivity {
 
